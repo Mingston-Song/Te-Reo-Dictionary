ec4a93ea8666b0f12f825fef613eb74ed51d25bf
Mingston-Song <20392@wc.school.nz>
2024-04-11 14:20:22 +1200

    Made database page which displays all entries Made ability to filter by categories

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, request, session
 import sqlite3
 from flask_bcrypt import Bcrypt
 
-DATABASE = "C:/Users/20392/OneDrive - Wellington College/Smile/smile.db"
+DATABASE = "database.db"
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "kkjsdlrft5uew2wweruyeou9iweqriurrmionmu9buhybvt7tfrct5rxcerxsweqa2qa3was5etdc6rv67tgb8yyh98n9nm0imj0ijku9uh7vgb6t7f6fd56d54"
@@ -20,6 +20,36 @@ def connect(db_file):
 @app.route('/')
 def render_home():
     return render_template('home.html')
+
+
+@app.route('/database')
+def render_database():
+    con = connect(DATABASE)
+    query = "SELECT entries.maori, entries.english, entries.definition, entries.image, categories.name FROM entries INNER JOIN categories ON entries.category_id=categories.id"
+    cur = con.cursor()
+    cur.execute(query)
+    entry_list = cur.fetchall()
+    query = "SELECT id, name FROM categories"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    con.close()
+    return render_template('database.html', entries=entry_list, categories=category_list)
+
+
+@app.route('/database/<category_id>')
+def render_database_categories(category_id):
+    con = connect(DATABASE)
+    query = "SELECT entries.maori, entries.english, entries.definition, entries.image, categories.name FROM entries INNER JOIN categories ON entries.category_id=categories.id WHERE category_id=?"
+    cur = con.cursor()
+    cur.execute(query, (category_id,))
+    entry_list = cur.fetchall()
+    query = "SELECT id, name FROM categories"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    con.close()
+    return render_template('database.html', entries=entry_list, categories=category_list)
 
 
 if __name__ == '__main__':
