4fdb2b27d9f80fb40d05c77c0333f07c450e7bcb
Exonuts <20392@wc.school.nz>
2024-05-14 22:39:38 +1200

    Finished add entry (untested) Fixed CSS issues

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ def logged_in():  # checks whether you are logged in
 
 @app.context_processor
 def admin_context_processor():
-    return dict(admin=admin)
+    return dict(is_admin=is_admin)
 
 
-def admin():
+def is_admin():
     if logged_in() and session.get("type") == 1:
         return True
     else:
@@ -49,9 +49,9 @@ def render_home():
 @app.route('/database')
 def render_database():
     con = connect(DATABASE)
-    query = """SELECT entries.id, entries.maori, entries.english, entries.definition, entries.level, categories.name 
-    FROM entries 
-    INNER JOIN categories ON entries.category_id=categories.id"""
+    query = "SELECT entries.id, entries.maori, entries.english, entries.definition, entries.level, categories.name " \
+            "FROM entries " \
+            "INNER JOIN categories ON entries.category_id=categories.id"
     cur = con.cursor()
     cur.execute(query)
     entry_list = cur.fetchall()  # fetch all the rows which matches the query
@@ -66,20 +66,36 @@ def render_database():
 @app.route('/database/<category_id>')
 def render_database_categories(category_id):
     con = connect(DATABASE)
-    query = """SELECT entries.id, entries.maori, entries.english, entries.definition, entries.level, categories.name 
-    FROM entries 
-    INNER JOIN categories ON entries.category_id=categories.id 
-    WHERE category_id=?"""
+    query = "SELECT entries.id, entries.maori, entries.english, entries.definition, entries.level, categories.name " \
+            "FROM entries " \
+            "INNER JOIN categories ON entries.category_id=categories.id " \
+            "WHERE category_id=?"
     cur = con.cursor()
     cur.execute(query, (category_id,))
     entry_list = cur.fetchall()
-    query = """SELECT id, name 
-    FROM categories"""
+    query = "SELECT id, name " \
+            "FROM categories"
     cur = con.cursor()
     cur.execute(query)
     category_list = cur.fetchall()
     con.close()
     return render_template('database.html', entries=entry_list, categories=category_list)
+
+
+@app.route('/search_type_mythicals', methods=['GET', 'Post'])
+def render_search_type_mythicals():
+    search = request.form['type_search']
+    query = "(Type LIKE ? or Second_Type LIKE ?)"
+    search = "%" + search + "%"
+    con = connect(DATABASE)
+    cur = con.cursor()
+    cur.execute(query, (search, search))
+    tag_list = cur.fetchall()
+    con.close()
+    if len(tag_list) == 0:
+        return redirect("/no_results")
+    else:
+        return render_template("mythicals.html", tags=tag_list)
 
 
 @app.route('/login', methods=["POST", "GET"])
@@ -182,46 +198,41 @@ def render_entry(entry_id):
 
 @app.route('/admin', methods=["POST", "GET"])
 def render_admin_page():
-    if not logged_in():
-        flash("You must be logged in")
+    if not is_admin():
+        flash("You must be an admin")
         return redirect("/")
+    con = connect(DATABASE)
+    query = "SELECT id, name FROM categories"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    con.close()
     if request.method == "POST":
         success = True
         print(request.form)
-        f_name = request.form.get("f_name").title().strip()
-        l_name = request.form.get("l_name").title().strip()
-        email = request.form.get("email").lower().strip()
-        password = request.form.get("password")
-        password2 = request.form.get("password2")
-        teacher = request.form.get("type")
-        if teacher == 'on':
-            account_type = 1
-        else:
-            account_type = 0
-        if password != password2:
-            flash("Passwords do not match")
-            success = False
-        if len(password) < 8:
-            flash("Password must be at least 8 characters")
-            success = False
-        hashed_password = bcrypt.generate_password_hash(password)
+        maori = request.form.get("maori").lower().strip()
+        english = request.form.get("english").lower().strip()
+        definition = request.form.get("definition").capitalize().strip()
+        level = request.form.get("level")
+        category_id = request.form.get("password2")
+        user_id = session.get("user_id")
         con = connect(DATABASE)
-        query = "INSERT INTO users (f_name, l_name, email, password, type) VALUES (?, ?, ?, ?, ?)"
+        query = "INSERT INTO entries (maori, english, definition, level, category_id, user_id) VALUES (?, ?, ?, ?, ?, ?)"
         cur = con.cursor()
         try:
-            cur.execute(query, (f_name, l_name, email, hashed_password, account_type))
+            cur.execute(query, (maori, english, definition, level, category_id, user_id))
         except sqlite3.IntegrityError:
             con.close()
-            flash("Email is already used")
+            flash("Failed to add new entry. Please check that the data you've entered meets the requirements.")
             success = False
         if success:
             con.commit()
             con.close()
-            return redirect("/login")
+            return redirect("/database")
         else:
             con.close()
-            return redirect("/signup")
-    return render_template('signup.html')
+            return redirect("/admin")
+    return render_template('admin.html', categories=category_list)
 
 
 if __name__ == '__main__':
